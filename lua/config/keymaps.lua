2fe5e579bc12408af047df9f3ca7a82e685f53dd
r.cody <ryan.p.cody@gmail.com>
2026-03-03 22:37:59 -0500

    laptop commits

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<F5>", function()
+  vim.cmd("w") -- save current file
+  vim.cmd("terminal dotnet build && /c/godot/editors/Godot_v4.6-stable_mono_win64/Godot_v4.stable_mono_win64")
+end, { desc = "Build + Run Godot" })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
-  { "folke/tokyonight.nvim" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "rebelot/kanagawa.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
 }
